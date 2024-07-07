9d5f3003a825e074be8e915ba2f9fe6695d4aab5
NehaChoudhary6 <157963172+NehaChoudhary6@users.noreply.github.com>
2024-07-07 22:22:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+            function purchase() {
+              alert("Hello! Enter your details, fees-399rs");
+              prompt("Enter your name- ");
+              prompt("Enter your phone number- ")
+              
+            }
+            
